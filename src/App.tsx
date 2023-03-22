030a330a0a9f584525fbb32bdfb6167fe70e0c3b
Souny261 <thongphetmlv@gmail.com>
2023-03-22 16:36:47 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CD/CI Version 0.0.5 Hello World NEW - Version : { import.meta.env.VITE_REACT_APP_VERSION}</h1>
+      <h1>Hello World NEW - Version : { import.meta.env.VITE_REACT_APP_VERSION}</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
